ddff3f74d388d69e9be1fff53529ff186f3ff090
AvinashKalmegh <avinashkalmegh93@gmail.com>
2022-10-01 21:14:58 +0530

    added

---
+++ scripts/register-AVINASH.js
@@ -0,0 +1,28 @@
+let form = document.getElementById("form");
+let user_data = [];
+  form.addEventListener("submit", () => {
+    event.preventDefault();
+
+    let first_name = document.getElementById("first_name").value;
+    let last_name = document.getElementById("last_name").value;
+    let email = document.getElementById("email").value;
+    let password = document.getElementById("password").value;
+
+    
+    let user_obj = {
+      first_name: first_name,
+      last_name: last_name,
+      email: email,
+      password: password,
+    };
+    user_data.push(user_obj);
+    //console.log(user_data);
+    if(user_obj.firstname=="" || user_obj.lastname=="" || user_obj.email=="" || user_obj.password ==""){
+      alert("Field is Empty");
+      return
+  }
+
+    localStorage.setItem("user_data", JSON.stringify(user_data));
+    alert("You are successfully registerd !")
+    window.location.href = "login.html"
+  });
---
+++ scripts/register.js
@@ -0,0 +1,22 @@
+let form = document.getElementById("form");
+let user_data = [];
+  form.addEventListener("submit", () => {
+    event.preventDefault();
+
+    let first_name = document.getElementById("first_name").value;
+    let last_name = document.getElementById("last_name").value;
+    let email = document.getElementById("email").value;
+    let password = document.getElementById("password").value;
+
+    
+    let user_obj = {
+      first_name: first_name,
+      last_name: last_name,
+      email: email,
+      password: password,
+    };
+    user_data.push(user_obj);
+    //console.log(user_data);
+
+    localStorage.setItem("user_data", JSON.stringify(user_data));
+  });
